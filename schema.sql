0bcde7d3c3bc637fa4165e628ef2fc98d516bfa6
Dmambo <mambotrader0@gmail.com>
2023-03-02 15:33:16 +0530

    Joined data

--- schema.sql
+++ schema.sql
@@ -39,3 +39,25 @@ ALTER TABLE animals
 ALTER TABLE animals
     ADD FOREIGN KEY (species) REFERENCES species(name);
 
+
+
+-- create vets table to the database
+CREATE TABLE vets (
+    id serial PRIMARY KEY,
+    name VARCHAR(100),
+    age INT,
+    date_of_graduation DATE
+);
+
+-- create specializations join table to the database
+CREATE TABLE specializations (
+    species_id INT REFERENCES species(id),
+    vet_id INT REFERENCES vets(id)
+);
+
+-- create visits table to join animals table and vets table
+CREATE TABLE visits (
+    animal_id INT REFERENCES animals(id),
+    vet_id INT REFERENCES vets(id),
+    visited_date date NOT NULL DEFAULT CURRENT_DATE
+);
