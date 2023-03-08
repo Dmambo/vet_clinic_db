e3fc36f1700561bef33c2ece433e15758fbed012
Fahim-Khan-P <fahim.tanvir2021@gmail.com>
2023-03-08 12:54:57 +0430

    Add medical_histoies table to the clinic DB!

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -5,3 +5,10 @@ CREATE TABLE patients (
     name VARCHAR(50),
     date_of_birth DATE
 );
+
+CREATE TABLE medical_histories (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    patient_id INT REFERENCES patients(id),
+    status VARCHAR(50),
+    admitted_at TIMESTAMP,
+);
