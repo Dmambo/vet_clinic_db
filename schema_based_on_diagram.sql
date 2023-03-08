8644b4f7249302e25927f36e52d7ae231353b654
Fahim-Khan-P <fahim.tanvir2021@gmail.com>
2023-03-08 12:55:48 +0430

    Create invoice tabel!

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -12,3 +12,10 @@ CREATE TABLE medical_histories (
     status VARCHAR(50),
     admitted_at TIMESTAMP,
 );
+CREATE TABLE invoices (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    total_amount DECIMAL(10, 2),
+    generate_at TIMESTAMP,
+    payed_at TIMESTAMP,
+    medical_history_id INT REFERENCES medical_histories(id)
+);
